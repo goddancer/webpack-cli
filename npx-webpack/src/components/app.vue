068ec924e3851df51189b9fdefd813ed6c42199d
goddancer <goddancer@foxmail.com>
2018-11-27 17:44:35 +0800

    test prettier

--- npx-webpack/src/components/app.vue
+++ npx-webpack/src/components/app.vue
@@ -1,38 +1,40 @@
 <template>
-    <div class="app">
-        <img src="../assets/bg.jpeg" alt="">
-        <div class="rotate"></div>
-        <div class="tt">123123123</div>
-    </div>
+  <div class="app">
+    <img src="../assets/bg.jpeg"
+         alt="">
+    <div class="rotate"></div>
+    <div class="tt border_top_1px">123123123</div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "app",
-        data() {
-            return {}
-        },
-        methods: {},
-        mounted() {
-        },
-    }
+  export default {
+    name: "app",
+    data() {
+      return {}
+    },
+    methods: {},
+    mounted() {
+    },
+  }
 </script>
 
 <style lang="less" scoped>
-    .app{
-        width: 100%;
-        height: 100%;
-        background: url("../assets/bg.jpeg") no-repeat center top / 100% 100%;
-        .rotate{
-            width: 75px;
-            height: 75px;
-            background: #c0c;
-            animation: rotate linear 1.5s infinite;
-        }
-        .tt{
-            line-height: 75px;
-            font-size: 50px;/*nor*/
-            color: #fff;
-        }
+  @import '~border-1px.less';
+
+  .app {
+    width: 100%;
+    height: 100%;
+    //background: url("../assets/bg.jpeg") no-repeat center top / 100% 100%;
+    .rotate {
+      width: 75px;
+      height: 75px;
+      background: #c0c;
+      animation: rotate linear 1.5s infinite;
     }
+    .tt {
+      line-height: 75px;
+      font-size: 50px; /*nor*/
+    }
+  }
 </style>
