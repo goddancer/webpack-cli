71b2f1b48a4f37f850a1269662207635b5aeb12c
goddancer <goddancer@foxmail.com>
2018-11-27 16:17:26 +0800

    lock scale when use postcss-px2rem;

--- npx-webpack/.postcssrc.js
+++ npx-webpack/.postcssrc.js
@@ -5,15 +5,15 @@ module.exports = {
     'postcss-import': {},
     'postcss-url': {},
     autoprefixer: {},
-      /*'xianyukeji-postcss-px2rem': {
-        remUnit: 37.5,
-        remResetMeidas: [
-          {
-            propertys: ['font-size', 'line-height'],
-            media: 'screen and (max-width: 374px)',
-          },
-        ],
-        remResetPropertys: [/^border(-)?(top|bottom|left|right)?(-)?(width)?$/],
-      },*/
+    'xianyukeji-postcss-px2rem': {
+      remUnit: 37.5,
+      remResetMeidas: [
+        {
+          propertys: ['font-size', 'line-height'],
+          media: 'screen and (max-width: 374px)',
+        },
+      ],
+      remResetPropertys: [/^border(-)?(top|bottom|left|right)?(-)?(width)?$/],
+    },
   },
 }
