8f7007efc539a87953f454b11346633431d8b9ee
goddancer <goddancer@foxmail.com>
2018-11-28 10:46:46 +0800

    add prettier&lint-staged&husky to lint code precommit

--- npx-webpack/README.md
+++ npx-webpack/README.md
@@ -1,15 +1,18 @@
 # npx-webpack
 
-- webpack4不设置--mode为development的时候，代码会自动压缩
+- webpack4 不设置--mode 为 development 的时候，代码会自动压缩
 
-- webpack默认配置结构
+- webpack 默认配置结构
+
 ```javascript
 module.exports = {
-    entry: '',               // 入口文件
-    output: {},              // 出口文件
-    module: {},              // 处理对应模块
-    plugins: [],             // 对应的插件
-    devServer: {},           // 开发服务器配置
-    mode: 'development'      // 模式配置
+  entry: '', // 入口文件
+  output: {}, // 出口文件
+  module: {}, // 处理对应模块
+  plugins: [], // 对应的插件
+  devServer: {}, // 开发服务器配置
+  mode: 'development', // 模式配置
 }
 ```
+
+npm install --save-dev lint-staged husky
--- npx-webpack/src/components/app.vue
+++ npx-webpack/src/components/app.vue
@@ -1,50 +1,55 @@
 <template>
   <div class="app">
-    <img src="../assets/bg.jpeg"
-         alt="">
+    <img src="../assets/bg.jpeg" alt="" />
     <div class="rotate"></div>
     <div class="tt border_top_1px">123123123</div>
   </div>
 </template>
 
 <script>
-  export default {
-    name: "app",
-    data() {
-      return {}
-    },
-    methods: {},
-    mounted() {
-      const a = ['123123123123123123123123123123123123123123123123123123', '123123123123123123123123123', '123123123123123123123123123', '123123123123123123123123123']
-      /*eslint-disable-next-line*/
-      console.log(a)
-      const foo = () => (false)
-      const reallyLongArg = () => (false)
-      const omgSoManyParameters = () => (false)
-      const IShouldRefactorThis = () => (false)
-      const isThereSeriouslyAnotherOne = () => (false)
+export default {
+  name: 'app',
+  data() {
+    return {}
+  },
+  methods: {},
+  mounted() {
+    const a = [
+      '123123123123123123123123123123123123123123123123123123',
+      '123123123123123123123123123',
+      '123123123123123123123123123',
+      '123123123123123123123123123',
+    ]
 
-      foo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne());
-    },
-  }
+    /*eslint-disable-next-line*/
+    console.log(a)
+    const foo = () => false
+    const reallyLongArg = () => false
+    const omgSoManyParameters = () => false
+    const IShouldRefactorThis = () => false
+    const isThereSeriouslyAnotherOne = () => false
+
+    foo(reallyLongArg(), omgSoManyParameters(), IShouldRefactorThis(), isThereSeriouslyAnotherOne())
+  },
+}
 </script>
 
 <style lang="less" scoped>
-  @import '~border-1px.less';
+@import '~border-1px.less';
 
-  .app {
-    width: 100%;
-    height: 100%;
-    //background: url("../assets/bg.jpeg") no-repeat center top / 100% 100%;
-    .rotate {
-      width: 75px;
-      height: 75px;
-      background: #c0c;
-      animation: rotate linear 1.5s infinite;
-    }
-    .tt {
-      line-height: 75px;
-      font-size: 50px; /*nor*/
-    }
+.app {
+  width: 100%;
+  height: 100%;
+  //background: url("../assets/bg.jpeg") no-repeat center top / 100% 100%;
+  .rotate {
+    width: 75px;
+    height: 75px;
+    background: #c0c;
+    animation: rotate linear 1.5s infinite;
   }
+  .tt {
+    line-height: 75px;
+    font-size: 50px; /*nor*/
+  }
+}
 </style>
